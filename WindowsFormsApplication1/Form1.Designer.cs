bec2e244100d44c2262d55d281b0ac4f8d39e5dd
Ruben <polouters@gmail.com>
2016-03-21 13:15:26 +0100

    version estable 1

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Files = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Analizar = new System.Windows.Forms.Button();
+            this.lista = new System.Windows.Forms.ListView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Files
+            // 
+            this.Files.Location = new System.Drawing.Point(12, 26);
+            this.Files.Name = "Files";
+            this.Files.Size = new System.Drawing.Size(771, 20);
+            this.Files.TabIndex = 0;
+            this.Files.Text = "M:\\PROYECTO\\75-European Projects\\7501 CDRGREFFE122015 Gestión de Archivos\\Entrega" +
+    "do por CoR\\Extraccion EXCEL CLARA Marzo 2016\\Extraction sauvegarde entree_805 RU" +
+    "BEN.xls";
+            this.Files.TextChanged += new System.EventHandler(this.File_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(310, 116);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "PROCESAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Analizar
+            // 
+            this.Analizar.Location = new System.Drawing.Point(348, 109);
+            this.Analizar.Name = "Analizar";
+            this.Analizar.Size = new System.Drawing.Size(310, 116);
+            this.Analizar.TabIndex = 2;
+            this.Analizar.Text = "ANALIZAR";
+            this.Analizar.UseVisualStyleBackColor = true;
+            this.Analizar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lista
+            // 
+            this.lista.Location = new System.Drawing.Point(12, 244);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(945, 460);
+            this.lista.TabIndex = 3;
+            this.lista.UseCompatibleStateImageBehavior = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(673, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(310, 116);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "QUITAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(883, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1038, 716);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lista);
+            this.Controls.Add(this.Analizar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Files);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Files;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Analizar;
+        private System.Windows.Forms.ListView lista;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+            string line;
+            ArrayList lineas = new ArrayList();
+            string archivo = Files.Text;
+
+            // Read the file and display it line by line.
+           
+            System.IO.StreamReader file = new System.IO.StreamReader(archivo);
+            
+            while ((line = file.ReadLine()) != null)
+            {
+                lineas.Add(line);
+              //  MessageBox.Show(line);
+                counter++;
+            }
+            file.Close();
+         //   MessageBox.Show("Cabecera: "+lineas[0].ToString());
+          
+            int ncol;
+            string[] bar1 = lineas[0].ToString().Split('\t');
+            ncol = lineas[0].ToString().Split('\t').Length;
+
+            string texto;
+            using (StreamReader sr = new StreamReader(archivo))
+            {
+                // Read the stream to a string, and write the string to the console.
+                texto = sr.ReadToEnd();
+           //    MessageBox.Show(texto);
+            }
+
+
+            MessageBox.Show(ncol.ToString());
+            string[] bar = texto.Split('\t');
+            
+            int i = 0;
+            int x = 0;
+            string nuevaL="";
+            while(x <bar.Length){
+            i++;
+            nuevaL += bar[x]+" ";
+                if (i == ncol)
+                {
+                    i = 0;
+                    MessageBox.Show(nuevaL);
+                    MessageBox.Show("Linea terminada");
+                    nuevaL = "";
+                }
+
+                
+                x++;
+ 
+            }
+            MessageBox.Show(nuevaL);
+            MessageBox.Show("Linea terminada");
+
+            
+
+
+        }
+
+        private void File_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lista.View = View.Details;
+            lista.GridLines = true;
+            lista.FullRowSelect = true;
+
+            string line;
+            string archivo = Files.Text;
+            int contador=0;
+
+            // Read the file and display it line by line.
+
+            System.IO.StreamReader file = new System.IO.StreamReader(archivo);
+            int ncampos = 0;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                if (contador == 0)
+                {
+                    // Cabecera
+                    string[] campos = line.ToString().Split('\t');
+                    string[,] lista_arr = new string[200, 2];
+
+                    lista.Clear();
+
+                    lista.Columns.Add("Nombre campo", 500);
+                    lista.Columns.Add("Long. Max.", 100);
+
+                    foreach (string campo in campos)
+                    {
+                        lista_arr[ncampos, 0] = campo;
+                        lista_arr[ncampos, 1] = "";
+                        ncampos++;
+                    }
+                    ListViewItem itm;
+
+           
+
+                    for (int i = 0; i < ncampos; i++)
+                    {
+                        string[] fila = new string[2];
+
+                        fila[0] = lista_arr[i, 0];
+                        fila[1] = lista_arr[i, 1];
+                        itm = new ListViewItem(fila);
+                        lista.Items.Add(itm);
+                    }
+                }
+                else
+                {
+                    // Fila de datos
+                    int camposprocesados = 0;
+                    string[] campos = line.ToString().Split('\t');
+                    do
+                    {
+                        foreach (string campo in campos)
+                        {
+                            // AQUI VA TODA LA LOGICA DEL PROGRAMA
+
+                            camposprocesados++;
+                        }
+                        line = file.ReadLine();
+                        campos = line.ToString().Split('\t');
+                    } while (camposprocesados < ncampos);
+
+                }
+                MessageBox.Show(line);
+                contador++;
+            }
+            file.Close();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            lista.Items.RemoveAt(Int32.Parse(textBox1.Text));
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
