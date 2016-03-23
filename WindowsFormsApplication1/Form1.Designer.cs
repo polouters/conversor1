82b5849048415bfd89817cdc406b5aa907ec9626
Ruben <polouters@gmail.com>
2016-03-23 11:31:35 +0100

    inserta en access y crea la tabla, falta que meta los datos, y que haga bien la conversion de Asci a UTF8

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -32,12 +32,19 @@
             this.button1 = new System.Windows.Forms.Button();
             this.Analizar = new System.Windows.Forms.Button();
             this.lista = new System.Windows.Forms.ListView();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Files
             // 
-            this.Files.Location = new System.Drawing.Point(135, 46);
+            this.Files.Location = new System.Drawing.Point(165, 12);
             this.Files.Name = "Files";
             this.Files.Size = new System.Drawing.Size(771, 20);
             this.Files.TabIndex = 0;
@@ -49,7 +56,7 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.RosyBrown;
-            this.button1.Location = new System.Drawing.Point(83, 97);
+            this.button1.Location = new System.Drawing.Point(82, 132);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(310, 116);
             this.button1.TabIndex = 1;
@@ -60,7 +67,7 @@
             // Analizar
             // 
             this.Analizar.BackColor = System.Drawing.Color.LightGreen;
-            this.Analizar.Location = new System.Drawing.Point(643, 97);
+            this.Analizar.Location = new System.Drawing.Point(626, 132);
             this.Analizar.Name = "Analizar";
             this.Analizar.Size = new System.Drawing.Size(310, 116);
             this.Analizar.TabIndex = 2;
@@ -70,27 +77,100 @@
             // 
             // lista
             // 
-            this.lista.Location = new System.Drawing.Point(49, 244);
+            this.lista.Location = new System.Drawing.Point(51, 280);
             this.lista.Name = "lista";
             this.lista.Size = new System.Drawing.Size(945, 460);
             this.lista.TabIndex = 3;
             this.lista.UseCompatibleStateImageBehavior = false;
             // 
-            // progressBar1
+            // textBox1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(467, 206);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(100, 23);
-            this.progressBar1.TabIndex = 4;
-            this.progressBar1.StyleChanged += new System.EventHandler(this.button2_Click);
+            this.textBox1.Location = new System.Drawing.Point(165, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(432, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "C:\\Users\\alberto\\Documents\\database1.accdb";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(6, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 18);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Archivo a procesar";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(6, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 18);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Archivo Access";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(165, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(574, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(6, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 18);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Titulo de la tabla";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(942, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(26, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_2);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(603, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(26, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.ClientSize = new System.Drawing.Size(1038, 716);
-            this.Controls.Add(this.progressBar1);
+            this.ClientSize = new System.Drawing.Size(1048, 766);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.lista);
             this.Controls.Add(this.Analizar);
             this.Controls.Add(this.button1);
@@ -108,7 +188,14 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button Analizar;
         private System.Windows.Forms.ListView lista;
-        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Data.OleDb;
 
 namespace WindowsFormsApplication1
 {
@@ -28,11 +29,61 @@ namespace WindowsFormsApplication1
             // prueba con for
             for (int i = 0; i < lista.Items.Count; i++)
             {
-             MessageBox.Show(lista.Items[i].SubItems[0].Text+ " tiene un tamaño de " +lista.Items[i].SubItems[1].Text);
+            // MessageBox.Show(lista.Items[i].SubItems[0].Text+ " tiene un tamaño de " +lista.Items[i].SubItems[1].Text);
             }
             //fin del for
 
-
+            OleDbConnection conn = new OleDbConnection();
+            conn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + textBox1.Text;
+            MessageBox.Show(conn.ConnectionString);
+            conn.Open();
+            string tableName = "["+textBox2.Text.Replace(".","_")+"]";
+            OleDbCommand cmmd = new OleDbCommand("", conn);
+            cmmd.CommandText = "CREATE TABLE " + tableName + "(";
+            for (int i = 0; i < lista.Items.Count; i++)
+            {
+                //camabiamos codificacion del texto para quitar posibles errores al llegar en ASCI
+                string myString = lista.Items[i].SubItems[0].Text.Replace(".", "_");
+                byte[] bytes = Encoding.Default.GetBytes(myString);
+                myString = Encoding.UTF8.GetString(bytes);
+                
+                if (i == 0)
+                {
+                    cmmd.CommandText += "[" + myString + "] Text Primary Key,";
+                }
+                else
+                {if(i == lista.Items.Count - 1)
+                    {
+                        cmmd.CommandText += "[" + myString + "] Text)";
+                    }
+                    else
+                    {
+                        cmmd.CommandText += "[" + myString + "] Text,";
+                    }
+                   
+                }
+                // MessageBox.Show(lista.Items[i].SubItems[0].Text + " tiene un tamaño de " + lista.Items[i].SubItems[1].Text);
+            }
+         
+           // cmmd.CommandText = "CREATE TABLE " + tableName + "( [I D] Counter Primary Key, [FirstName] Text, [LastName] Text, [Gender] Text, [Phone] Text, [CellPhone] Text, [FriendsFirstName] Text, [FriendsLastName] Text, [RegisterDate] Text, [Size] Text, [Description] Text)";
+            if (conn.State == ConnectionState.Open)
+            {
+                try
+                {
+                    cmmd.ExecuteNonQuery();
+                    MessageBox.Show("Add!");
+                    conn.Close();
+                }
+                catch (OleDbException expe)
+                {
+                    MessageBox.Show(expe.Message);
+                    conn.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error!");
+            }
 
 
         }
@@ -61,7 +112,7 @@ namespace WindowsFormsApplication1
             //   string[,] lista_arr = new string[ncampos, 2];
 
 
-
+            
             // Cabecera
             line = file.ReadLine();
             string[] campos = line.ToString().Split('\t');
@@ -96,17 +147,17 @@ namespace WindowsFormsApplication1
                     if (concatenar == 1)
                     {
                         registro[registro.Count-1] += "\n" + campo; 
-                        Console.WriteLine("Concatenar con la anterior");
+                  //      Console.WriteLine("Concatenar con la anterior");
                         concatenar = 0;
                     }
                     else
                     {
                         registro.Add(campo);
                     }
-                    Console.WriteLine(camposprocesados.ToString() + " - " + campo);
+                //    Console.WriteLine(camposprocesados.ToString() + " - " + campo);
                     camposprocesados++;
                 }
-               Console.WriteLine(camposprocesados + "/" + ncampos);
+              // Console.WriteLine(camposprocesados + "/" + ncampos);
                 
                 if (camposprocesados == ncampos) {
                     //Antes del siguiente registro hacemos una repetitiva para revisar el tema del Tamaño
@@ -145,8 +196,31 @@ namespace WindowsFormsApplication1
             file.Close();
         }
 
-        
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            
+        }
 
-        
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_2(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                Files.Text = openFileDialog1.FileName;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                textBox1.Text = openFileDialog1.FileName;
+        }
     }
 }
